100d11f00d674be25f82505f9993a15e96676bd9
jackyzy823 <jackyzy823@gmail.com>
2020-03-22 05:48:30 -0700

    update init.sh to correct file check logic and update firefox android config guide

--- init.sh
+++ init.sh
@@ -1,9 +1,21 @@
 set -a
-[ -f .env ] && . .env || (echo -e "\e[31mCannot Found .env\e[0m" && exit -1)
+if [ -f .env ] && . .env ;then
+	echo -e "\e[32mFound .env\e[0m"
+else
+	echo -e "\e[31mCannot Found .env\e[0m"
+	exit -1
+fi
 set +a
+
 echo -e "\e[32mPlease ensure that you configured subdomains and their certs: www.$DOMAIN_NAME profile.$DOMAIN_NAME token.$DOMAIN_NAME  api.$DOMAIN_NAME oauth.$DOMAIN_NAME (api.$DOMAIN_NAME and oauth.$DOMAIN_NAME must use same cert) \e[0m"
 # do this to ensure .env 's   PERSISTENCEPATH  relate to docker-compose.yml
-[ -f docker-compose.yml ] || (echo "Please run under the folder contains docker-compose.yml" && exit -1)
+if [ -f docker-compose.yml ] ;then
+	:
+else
+	echo "Please run under the folder contains docker-compose.yml"
+	exit -1
+fi
+
 echo -e "\e[32mmake folder $PERSISTENCEPATH/public and set it all writalbe for profile avatar\e[0m"
 mkdir -p $PERSISTENCEPATH/public && chmod a+w $PERSISTENCEPATH/public
 echo -e "\e[32mmake folder $PERSISTENCEPATH/mysql_data for mysql\e[0m"
@@ -13,9 +25,19 @@ mkdir -p $PERSISTENCEPATH/mysql_data
 if [ ! -f wait ] ;then 
 	echo -e "\e[32mDownloading docker-compose-wait from https://github.com/ufoscout/docker-compose-wait\e[0m"
 	if [ -x "$(command -v wget)" ]; then
-		wget -O wait https://github.com/ufoscout/docker-compose-wait/releases/download/2.7.3/wait || (echo -e "\e[31mDownload docker-compose-wait failed!\e[0m" && exit -1)
+		if wget -O wait https://github.com/ufoscout/docker-compose-wait/releases/download/2.7.3/wait ;then
+			echo -e "\e[32mDownload docker-compose-wait successfully!\e[0m"
+		else
+			echo -e "\e[31mDownload docker-compose-wait failed!\e[0m"
+			exit -1
+		fi
 	elif [ -x "$(command -v curl)" ]; then
-		curl -o wait https://github.com/ufoscout/docker-compose-wait/releases/download/2.7.3/wait || (echo -e "\e[31mDownload docker-compose-wait failed!\e[0m" && exit -1)
+		if curl -o wait https://github.com/ufoscout/docker-compose-wait/releases/download/2.7.3/wait ;then
+			echo -e "\e[32mDownload docker-compose-wait successfully!\e[0m"
+		else
+			echo -e "\e[31mDownload docker-compose-wait failed!\e[0m"
+			exit -1	
+		fi		
 	else
 		echo -e "\e[31mCannot Download docker-compose. Please manually download it.\e[0m" 
 		exit -1
@@ -38,6 +60,22 @@ cat <<HERE
 HERE
 
 echo -e "\e[0m" #reset
+
+echo -e "\e[32m Config for Firefox android\e[0m"
+
+echo -e "\e[33m" 
+cat <<HERE
+"identity.fxaccounts.auth.uri":"https://api.$DOMAIN_NAME/v1",
+"identity.fxaccounts.remote.oauth.uri":"https://oauth.$DOMAIN_NAME/v1",
+"identity.fxaccounts.remote.profile.uri":"https://profile.$DOMAIN_NAME/v1",
+"identity.fxaccounts.remote.webchannel.uri":"https://www.$DOMAIN_NAME",
+
+Add https://www.$DOMAIN_NAME to "webchannel.allowObject.urlWhitelist"
+
+HERE
+
+echo -e "\e[0m" #reset
+
 
 echo -e "\e[32m Check sigincode \e[0m"
 
