21c818a570ae7d0b94bb1cdd4b2e6077428899e2
jackyzy823 <jackyzy823@gmail.com>
2021-03-06 12:17:15 +0000

    add script to get firefox production release number

---
+++ tests/product_release_version.py
@@ -0,0 +1,8 @@
+import re
+import json
+from urllib.request import urlopen
+from urllib.parse import unquote_plus
+
+page = urlopen("https://accounts.firefox.com").read()
+cfg = re.findall(r'''name=fxa-content-server/config content=(.*?)>''',page.decode())[0]
+print(json.loads(unquote_plus(cfg)).get("release"))
