b7d57ceaa019e673f6ca47a451820a0ac6accf83
jackyzy823 <jackyzy823@gmail.com>
2021-09-25 06:17:52 +0200

    fix nginx subst path error, update version check script

--- tests/product_release_version.py
+++ tests/product_release_version.py
@@ -1,8 +1,12 @@
-import re
+#import re
 import json
 from urllib.request import urlopen
-from urllib.parse import unquote_plus
+#from urllib.parse import unquote_plus
 
-page = urlopen("https://accounts.firefox.com").read()
-cfg = re.findall(r'''name=fxa-content-server/config content=(.*?)>''',page.decode())[0]
-print(json.loads(unquote_plus(cfg)).get("release"))
+v = json.loads(urlopen("https://api.accounts.firefox.com/").read())
+print(v)
+print(v.get("version"))
+
+#page = urlopen("https://accounts.firefox.com").read()
+#cfg = re.findall(r'''name=fxa-content-server/config content=(.*?)>''',page.decode())[0]
+#print(json.loads(unquote_plus(cfg)).get("release"))
